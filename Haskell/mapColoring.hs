b03845fb381f851f837a383ccbd6290fd5d9b5d6
Jerry2001 <ngohongphuc2001@gmail.com>
2020-12-02 16:48:38 -0600

    Add map coloring

---
+++ Haskell/mapColoring.hs
@@ -0,0 +1,21 @@
+module Main where
+
+colours = ["red", "green", "blue"]
+label = [((alabama, mississippi, georgia, tennessee, florida) |
+  alabama <- colours,
+  mississippi <- colours,
+  georgia <- colours,
+  tennessee <- colours,
+  florida <- colours,
+  mississippi /= tennessee,
+  mississippi /= alabama,
+  alabama /= tennessee,
+  alabama /= georgia,
+  alabama /= florida,
+  georgia /= florida,
+  georgia /= tennessee]
+
+main :: IO ()
+main = return ()
+
+-- :l mapColoring.hs
